cd9cb98dda7eaf8439670605a007fd52f769701a
asharma20 <as2837@cornll.edu>
2021-06-15 19:55:20 -0700

    Change load screen money value
    - Add option to copy save files

--- stardewvalleymod.py
+++ stardewvalleymod.py
@@ -1,24 +1,53 @@
 import argparse
 from argparse import RawTextHelpFormatter
+import os
 import re
+from distutils.dir_util import copy_tree
 
 def main(args):
-  filename = args.savefile
+  savedir = args.savedir
+  savename = os.path.basename(os.path.dirname(savedir))
+  filepath = os.path.join(savedir, savename)
+  savepath = os.path.join(savedir, 'SaveGameInfo')
+  
+  # Copy save directory
+  if (args.copy_save):
+    savesdir = os.path.dirname(os.path.dirname(savedir))
+    copynum = 1
+    copydir = os.path.join(savesdir, savename + '-' + str(copynum))
+    while os.path.exists(copydir):
+      copynum += 1
+      copydir = copydir[:-2]
+      copydir += '-' + str(copynum)
+    copy_tree(savedir, copydir)
 
-  with open(filename, 'r') as fin:
-    filestring = fin.read()
+  # Read game file, save file
+  with open(filepath, 'r') as fin:
+    newdata = fin.read()
+  with open(savepath, 'r') as fin:
+    savedata = fin.read()
 
-  newdata = re.sub(r'<money>\d*', r'<money>' + str(args.money), filestring)
+  # Modify data
+  if args.money:
+    newdata = re.sub(r'<money>\d*', r'<money>' + str(args.money), newdata)
+    savedata = re.sub(r'<money>\d*', r'<money>' + str(args.money), savedata)
 
-  with open(filename, 'w') as fout:
+  # Overwrite game file, save file
+  with open(filepath, 'w') as fout:
     fout.write(newdata)
+  with open(savepath, 'w') as fout:
+    fout.write(savedata)
 
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
-  parser.add_argument("--savefile", required=True, help='Path location of save file (i.e. Replace Farm_123456789 with your save name,'
-    '\n Windows: "%%AppData%%\StardewValley\Saves\Farm_123456789\Farm_123456789",' 
-    '\n Mac:     "~/.config/StardewValley/Saves/Farm_123456789/Farm_123456789")')
-  parser.add_argument("--money", default=99999999, help='Amount of money to set, default: 99999999')
+  parser.add_argument('--savedir', required=True, help='Path location of save directory.'
+    '\n (i.e. Replace Farm_123456789 with your save name,'
+    '\n Windows: "%%AppData%%\StardewValley\Saves\Farm_123456789",' 
+    '\n Mac:     "~/.config/StardewValley/Saves/Farm_123456789")')
+  parser.add_argument('--copy_save', action='store_true', help='Create a copy of the save before modifying.'
+    '\n It is recommended to copy original save at least once.'
+    '\n Note: this will keep adding new copies even when previous copies exist.')
+  parser.add_argument('--money', type=int, help='Amount of money to set')
   args = parser.parse_args()
   main(args)
